520d6670ae2042796accd180a259d7bf9fb1f034
wi23b <wi23b174@technikum-wien.at>
2024-09-08 14:37:08 +0200

    Update Main.java

--- src/main/java/at/technikum/Main.java
+++ src/main/java/at/technikum/Main.java
@@ -1,7 +1,19 @@
 package at.technikum;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private void printBoard() {
+        System.out.println("-------------");
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println("\n-------------");
+        }
     }
-}
+
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.play();
+    }
+} 
